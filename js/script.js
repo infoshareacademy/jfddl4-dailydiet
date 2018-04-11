73fc3df198d7e3d0084b31086f810c607d2cb5a7
madzia89 <wojcicka.ma@gmail.com>
2018-04-11 17:02:46 +0200

    slider init

--- js/script.js
+++ js/script.js
@@ -10,3 +10,36 @@ $('a.smooth-scroll').on('click', function(event){
         scrollTop: $($.attr(this, 'href')).offset().top - 30
     }, 800);
 });
+
+
+carousel = (function(){
+    var box = document.querySelector('.hero__carousel-box');
+    var next = box.querySelector('.next');
+    var prev = box.querySelector('.prev');
+    var items = box.querySelectorAll('.hero__carousel-pictures hero__carousel-items');
+    var counter = 0;
+    var amount = items.length;
+    var current = items[0];
+    box.classList.add('active');
+    function navigate(direction) {
+        current.classList.remove('current');
+        counter = counter + direction;
+        if (direction === -1 &&
+            counter < 0) {
+            counter = amount - 1;
+        }
+        if (direction === 1 &&
+            !items[counter]) {
+            counter = 0;
+        }
+        current = items[counter];
+        current.classList.add('current');
+    }
+    next.addEventListener('click', function(ev) {
+        navigate(1);
+    });
+    prev.addEventListener('click', function(ev) {
+        navigate(-1);
+    });
+    navigate(0);
+})();
