0eead28d802b1c3427b39ad7fb187790a9209115
madzia89 <wojcicka.ma@gmail.com>
2018-04-17 16:57:35 +0200

    add player

--- game/js/game-skeleton.js
+++ game/js/game-skeleton.js
@@ -9,51 +9,75 @@
     var _timeContainer = null
 
     // Set default player position
-    var _initialPlayerPositon = 1
-
-    // Create player with default position X
-    var _player = {
-        positionX: _initialPlayerPositon
-    }
-
-    var gameIntervals = [[checkCollision(), 500], [placeObstacle(), 3000]]
+    // var _initialPlayerPositon = 1
+    //
+    // // Create player with default position X
+    // var __player = null //= {
+    //     positionX: _initialPlayerPositon
+    // }
+    //
+    // var gameIntervals = [[checkCollision(), 500], [placeObstacle(), 3000]]
 
     // FUNCTIONS
 
     // game initial
 
     function gameInit(container) {
-        gameInstruction()
+        gameBoard(container)
+        // gameInstruction()
+        // placeObstacle()
         placePlayer()
-        placeObstacle()
-        gameBoard()
-        render()
-        attachEventListeners()
+        // render()
+        // attachEventListeners()
 
-        _timeInterval = function(element, index, array) {
-            for (var i =0; i < array.length; i++) {
-                setInterval(element[index][0], element[index][1])
-            }
-        }
+        // _timeInterval = function(element, index, array) {
+        //     for (var i =0; i < array.length; i++) {
+        //         setInterval(element[index][0], element[index][1])
+        //     }
+        // }
     }
 
+
+    //
     function gameBoard(container) {
         _gameBoard = document.createElement('div')
-        _scoreContainer = document.createElement('div')
-        _lifesContainer = document.createElement('div')
-        _timeContainer = document.createElement('div')
-        container.appendChild(_scoreContainer)
-        container.appendChild(_timeContainer)
-        container.appendChild(_lifesContainer)
+    //     _scoreContainer = document.createElement('div')
+    //     _lifesContainer = document.createElement('div')
+    //     _timeContainer = document.createElement('div')
+    //     container.appendChild(_scoreContainer)
+    //     container.appendChild(_timeContainer)
+    //     container.appendChild(_lifesContainer)
+        _gameBoard.style.position = 'relative'
+        _gameBoard.style.backgroundColor = 'red'
+        _gameBoard.style.width = '98vh'
+        _gameBoard.style.height = '98vh'
+        _gameBoard.style.margin = '0 auto'
+
         container.appendChild(_gameBoard)
-
-        render()
-
-        drawTime()
-        drawScore()
-        drawLifes()
-        //drawBackground()
+    //
+    //     render()
+    //
+    //     drawTime()
+    //     drawScore()
+    //     drawLifes()
+    //     //drawBackground()
     }
+
+    function placePlayer(){
+        __player = document.createElement('div')
+        __player.style.position = 'absolute'
+        __player.style.left = '45%'
+        __player.style.top = '85%'
+
+        __player.style.backgroundColor = 'blue'
+        __player.style.width = '10%'
+        __player.style.height = '10%'
+       _gameBoard.appendChild(__player)
+    }
+
+
+
+
 //////////////////////PLAYER POSITION////////////////////////
     function attachEventListeners() {
         document.addEventListener('keydown', function (event) {
@@ -80,7 +104,7 @@
         }
         render()
     }
-
+    //
 
 
 
@@ -103,6 +127,8 @@
     wydarzenia - np zebranie przedmiotu
     animacje*/
 
+
+    gameInit(document.body)
 })()
 
 
@@ -110,23 +136,3 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
