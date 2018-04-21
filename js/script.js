562836a8396ccaeb5f647a3f602006d20b88a158
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-04-21 14:17:25 +0200

    Add: + Back to top button's bottom position depends now on cookies section's display ('flex' or 'none') + Cursor pointer to cookie img

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//--- SMOOTH NAVIGATION SCROLL ---
 $(document).ready(function () {
     $('.menu').click(function () {
         $('.nav-list').toggleClass('active');
@@ -5,14 +6,14 @@ $(document).ready(function () {
 })
 
 $('a.smooth-scroll').on('click', function(event){
-    event.preventDefault();
+    event.preventDefault()
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - 30
-    }, 800);
-});
+    }, 800)
+})
+//--- / SMOOTH NAVIGATION SCROLL ---
 
 //--- TEMPORARY LOCAL STORAGE COOKIE ---
-
 if (!localStorage.getItem('cookie',)) {
     document.getElementById('cookies').style.display = 'flex'
 }
@@ -22,24 +23,36 @@ $('#cookies--close').on(
     function() {
         localStorage.setItem('cookie', true)
         document.getElementById('cookies').style.display = 'none'
+        changeButtonPosition()
     })
+//--- / TEMPORARY LOCAL STORAGE COOKIE ---
 
 //--- BACK TO TOP BUTTON ---
-
-$(document).ready(function(){
+$(document).ready(function() {
 
     //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 200) {
             document.getElementById('scrollToTop').style.display = 'flex'
         } else if ($(this).scrollTop() < 200) {
             document.getElementById('scrollToTop').style.display = 'none'
         }
-    });
+    })
 
     //Click event to scroll to top
-    $('.scrollToTop').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
-    });
-});
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({scrollTop : 0},800)
+        return false
+    })
+
+})
+
+// Adjust srollToTop button bottom position depending on 'cookies' display 'felx' or 'none'
+
+function changeButtonPosition() {
+    var cookiesHeight = document.getElementById('cookies').offsetHeight || 0
+    document.getElementById('scrollToTop').style.bottom = cookiesHeight + 35 + 'px'
+}
+
+changeButtonPosition()
+//--- / BACK TO TOP BUTTON ---
