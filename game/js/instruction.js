88a761ce52f8b57cfb305d50f9b1d9ff7386f471
madzia89 <wojcicka.ma@gmail.com>
2018-04-21 18:35:45 +0200

    init modal with instructions

--- game/js/instruction.js
+++ game/js/instruction.js
@@ -1,75 +1,19 @@
-(function () {
-    var _instructions = null
-    var _container = document.querySelector('body')
-    _container.style.background = "green"
+var modal = document.getElementById('instructions')
 
-    function init(container) {
-        instructions(container)
+var btn = document.getElementById("myBtn") //zamienić na btn z formularza
+
+var span = document.getElementsByClassName("modal__close")[0]
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-
-    function instructions(container) {
-        _instructions = document.createElement('div')
-        _instructions.style.width = '80%'
-        _instructions.style.margin = 'auto'
-        _instructions.style.background = 'rgba(255,255,255,0.7)'
-        container.appendChild(_instructions)
-
-        var titleDiv = document.createElement('div')
-        _instructions.appendChild(titleDiv)
-
-
-        var title = document.createElement('h1')
-        title.innerText = 'Thank you!'
-        title.style.textAlign = 'center'
-        title.style.color = 'darkGreen'
-        titleDiv.appendChild(title)
-
-        var titleParagraph = document.createElement('p')
-        titleParagraph.innerText = 'To thank for your subscription we invite you to enjoy Daily Diet game which encourages to live healthy life.'
-        titleParagraph.style.textAlign = 'center'
-        titleParagraph.style.fontSize = '22px'
-        titleDiv.appendChild(titleParagraph)
-
-
-        var hr = document.createElement('hr')
-        titleParagraph.appendChild(hr)
-
-        var rules = document.createElement('div')
-        rules.style.display = 'inline-block'
-        rules.style.float = 'right'
-        rules.style.width = '40%'
-        rules.style.marginRight = '10%'
-        container.appendChild(rules)
-
-
-        var arrowContainer = document.createElement('div')
-        arrowContainer.style.display = 'inline-block'
-        arrowContainer.style.marginLeft = '10%'
-        arrowContainer.style.width = '38%'
-        container.appendChild(arrowContainer)
-
-        var arrowsPic = document.createElement('img')
-        arrowsPic.setAttribute('src', 'img/twoArrows.png')
-        arrowsPic.style.position = 'relative'
-        arrowContainer.appendChild(arrowsPic)
-        var arrayWithInstructions = [
-            'use arrow keys to move left and right',
-            'catch healthy products to stay fit',
-            'avoid unhealthy food, otherwise your character is going to end miserably'
-        ]
-
-        var instructionList = document.createElement('ol')
-        rules.appendChild(instructionList)
-
-        arrayWithInstructions.forEach(function (thingsToDo) {
-            var li = document.createElement('li')
-            instructionList.appendChild(li)
-            li.innerHTML += thingsToDo
-        })
-        instructionList.style.fontSize = '22px'
-
-    }
-
-    init(_container)
-
-})()
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//--- SMOOTH NAVIGATION SCROLL ---
 $(document).ready(function () {
     $('.menu').click(function () {
         $('.nav-list').toggleClass('active');
@@ -6,68 +5,12 @@ $(document).ready(function () {
 })
 
 $('a.smooth-scroll').on('click', function(event){
-    event.preventDefault()
+    event.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - 30
-    }, 800)
-})
-//--- / SMOOTH NAVIGATION SCROLL ---
+    }, 800);
+});
 
-//--- TEMPORARY LOCAL STORAGE COOKIE ---
-if (!localStorage.getItem('cookie',)) {
-    document.getElementById('cookies').style.display = 'flex'
-}
 
-$('#cookies--close').on(
-    'click',
-    function() {
-        localStorage.setItem('cookie', true)
-        document.getElementById('cookies').style.display = 'none'
-        changeButtonPosition()
-    })
-//--- / TEMPORARY LOCAL STORAGE COOKIE ---
 
-//--- / TEAM MEMBERS ANIMATION ON IN-VIEW
-inView.offset(80)
 
-inView('.team_magda').on('enter', (function () {
-    $('.team__magda').addClass('team__member-picture__animation')
-}))
-
-inView('.team_szymon').on('enter', (function () {
-    $('.team__szymon').addClass('team__member-picture__animation')
-}))
-
-inView('.team_bartek').on('enter', (function () {
-    $('.team__bartek').addClass('team__member-picture__animation')
-}))
-//--- / TEAM MEMBERS ANIMATION ON IN-VIEW
-
-//--- BACK TO TOP BUTTON ---
-$(document).ready(function() {
-
-    //Check to see if the window is top if not then display button
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-            document.getElementById('scrollToTop').style.display = 'flex'
-        } else if ($(this).scrollTop() < 200) {
-            document.getElementById('scrollToTop').style.display = 'none'
-        }
-    })
-
-    //Click event to scroll to top
-    $('.scrollToTop').click(function() {
-        $('html, body').animate({scrollTop : 0},800)
-        return false
-    })
-
-})
-
-// Adjust srollToTop button bottom position depending on 'cookies' display 'felx' or 'none'
-function changeButtonPosition() {
-    var cookiesHeight = document.getElementById('cookies').offsetHeight || 0
-    document.getElementById('scrollToTop').style.bottom = cookiesHeight + 35 + 'px'
-}
-
-changeButtonPosition()
-//--- / BACK TO TOP BUTTON ---
